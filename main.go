7f678456f689e185ce313280ab16fade74c80d24
FEKA <fekalegi@gmail.com>
2020-11-02 13:59:15 +0700

    add route and listenserver to get

--- main.go
+++ main.go
@@ -1,28 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"github.com/gorilla/mux"
 	uuid "github.com/satori/go.uuid"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"log"
+	"net/http"
 )
 
 var tweet []Tweet
 
+var db, err = gorm.Open(sqlite.Open("tweets.db"), &gorm.Config{})
+
 type Tweet struct {
-	TweetID uuid.UUID `gorm:"type:uuid;primary_key;not null; json:"Tweetid"`
-	Username string `sql:"type:json" json:"Username"`
-	ContentText string `sql:"type:json" json:"Contenttext"`
-	ContentAttachment string `sql:"type:json" json:"Contentattachment"`
+	TweetID           uuid.UUID `gorm:"type:uuid;primary_key;not null; json:"Tweetid"`
+	Username          string    `sql:"type:json" json:"Username"`
+	ContentText       string    `sql:"type:json" json:"Contenttext"`
+	ContentAttachment string    `sql:"type:json" json:"Contentattachment"`
 }
 
-func main()  {
-
-	db, err := gorm.Open(sqlite.Open("tweets.db"), &gorm.Config{})
+func connectDB() *gorm.DB {
 	if err != nil {
 		panic("failed to connect database")
 	}
+	return db
+}
+
+func getTweet(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	result := []map[string]interface{}{}
 	db.Table("tweets").Find(&result)
-	fmt.Print(result)
+	json.NewEncoder(w).Encode(&result)
+	return
+}
+
+func main() {
+	r := mux.NewRouter()
+	connectDB()
+
+	r.HandleFunc("/getTweet", getTweet).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
