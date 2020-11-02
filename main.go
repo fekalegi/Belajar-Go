eef08b770ac614f821520a92cc3fdae332b8470a
FEKA <fekalegi@gmail.com>
2020-11-03 04:58:28 +0700

    adding del and specified get by id function

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	uuid "github.com/satori/go.uuid"
 	"gorm.io/driver/sqlite"
@@ -18,7 +17,7 @@ var db, err = gorm.Open(sqlite.Open("tweets.db"), &gorm.Config{})
 var tweets []Tweet
 
 type Tweet struct {
-	TweetID           uuid.UUID `gorm:"type:uuid;primary_key;not null; json:"Tweetid"`
+	TweetID           uuid.UUID `gorm:"type:uuid;primary_key;not null; json:"tweet_id"`
 	Username          string    `sql:"type:json" json:"username"`
 	ContentText       string    `sql:"type:json" json:"content_text"`
 	ContentAttachment string    `sql:"type:json" json:"content_attachment"`
@@ -44,14 +43,14 @@ func addTweet(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewDecoder(r.Body).Decode(&tweet)
 	tweet.TweetID = uuid.NewV4()
 	err := db.Create(&tweet)
-	if err != nil {
-		w.Write([]byte("error"))
-		fmt.Print(err)
+	if err.Error != nil {
+		w.Write([]byte("Error"))
+	} else {
+		w.Write([]byte("Sukses"))
 	}
-	w.Write([]byte("sukses"))
 }
 
-func getTweet(w http.ResponseWriter, r *http.Request) {
+func getAllTweet(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	result := []map[string]interface{}{}
 	db.Table("tweets").Find(&result)
@@ -59,12 +58,37 @@ func getTweet(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func getTweet(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var tweet Tweet
+	tweet.TweetID, _ = uuid.FromString(params["tweet_id"])
+	result := map[string]interface{}{}
+	db.Table("tweets").Where("tweet_id = ?", tweet.TweetID).Find(&result)
+	json.NewEncoder(w).Encode(&result)
+}
+
+func delTweet(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var tweet Tweet
+	tweet.TweetID, _ = uuid.FromString(params["tweet_id"])
+	err := db.Table("tweets").Delete("tweet_id ", tweet)
+	if err.Error != nil {
+		w.Write([]byte("Error"))
+	} else {
+		w.Write([]byte("Sukses"))
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 	connectDB()
 	Migrate()
 
-	r.HandleFunc("/Tweet", getTweet).Methods("GET")
+	r.HandleFunc("/Tweet", getAllTweet).Methods("GET")
 	r.HandleFunc("/Tweet", addTweet).Methods("POST")
+	r.HandleFunc("/Tweet/{tweet_id}", delTweet).Methods("DELETE")
+	r.HandleFunc("/Tweet/{tweet_id}", getTweet).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
